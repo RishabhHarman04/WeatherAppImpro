91ad249536d754837d81881265cf469277110d53
RishabhHarman04 <er.23rishabh@gmail.com>
2023-02-21 09:32:32 +0530

    All files

---
+++ WindowsFormsApp5/WeatherInfo.Designer.cs
@@ -0,0 +1,175 @@
+﻿namespace WindowsFormsApp5
+{
+    partial class WeatherInfo
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lblTemperature = new System.Windows.Forms.Label();
+            this.lblAtmosphere = new System.Windows.Forms.Label();
+            this.lblHumidity = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label4 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.Honeydew;
+            this.panel1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.lblTemperature);
+            this.panel1.Controls.Add(this.lblAtmosphere);
+            this.panel1.Controls.Add(this.lblHumidity);
+            this.panel1.Location = new System.Drawing.Point(156, 138);
+            this.panel1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(275, 333);
+            this.panel1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(-4, 0);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(51, 24);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Text";
+            // 
+            // lblTemperature
+            // 
+            this.lblTemperature.AutoSize = true;
+            this.lblTemperature.Font = new System.Drawing.Font("Microsoft YaHei", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTemperature.Location = new System.Drawing.Point(62, 280);
+            this.lblTemperature.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lblTemperature.Name = "lblTemperature";
+            this.lblTemperature.Size = new System.Drawing.Size(55, 21);
+            this.lblTemperature.TabIndex = 2;
+            this.lblTemperature.Text = "label3";
+            // 
+            // lblAtmosphere
+            // 
+            this.lblAtmosphere.AutoSize = true;
+            this.lblAtmosphere.Font = new System.Drawing.Font("Microsoft YaHei", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblAtmosphere.Location = new System.Drawing.Point(47, 181);
+            this.lblAtmosphere.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lblAtmosphere.Name = "lblAtmosphere";
+            this.lblAtmosphere.Size = new System.Drawing.Size(55, 21);
+            this.lblAtmosphere.TabIndex = 1;
+            this.lblAtmosphere.Text = "label2";
+            // 
+            // lblHumidity
+            // 
+            this.lblHumidity.AutoSize = true;
+            this.lblHumidity.Font = new System.Drawing.Font("Microsoft YaHei", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblHumidity.Location = new System.Drawing.Point(71, 94);
+            this.lblHumidity.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lblHumidity.Name = "lblHumidity";
+            this.lblHumidity.Size = new System.Drawing.Size(55, 21);
+            this.lblHumidity.TabIndex = 0;
+            this.lblHumidity.Text = "label1";
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 5000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 28F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(133, 6);
+            this.label4.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(337, 44);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Weather Forecast";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(231, 96);
+            this.comboBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(82, 21);
+            this.comboBox1.TabIndex = 4;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(235, 62);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(71, 17);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Wait Time";
+            // 
+            // WeatherInfo
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.LightYellow;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(856, 456);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.panel1);
+            this.DoubleBuffered = true;
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Name = "WeatherInfo";
+            this.Text = "Form1";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Form1_FormClosed);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label lblHumidity;
+        private System.Windows.Forms.Label lblTemperature;
+        private System.Windows.Forms.Label lblAtmosphere;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label2;
+    }
+}
---
+++ WindowsFormsApp5/WeatherInfo.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net.Http;
+using System.Net.Http.Headers;
+
+
+namespace WindowsFormsApp5
+{
+    public partial class WeatherInfo : Form
+    {
+        private int panelIndex = 1;
+        private HttpClient httpClient = new HttpClient();
+        private string apiKey = MyStrings.Apikey;
+        private Timer timer = new Timer();
+        private int selectedInterval = 10000;
+
+
+        public WeatherInfo()
+        {
+            InitializeComponent();
+            this.Load += Form1_Load;
+            comboBox1.Items.Add(MyStrings.five);
+            comboBox1.Items.Add(MyStrings.ten);
+            comboBox1.Items.Add(MyStrings.fifteen);
+            comboBox1.SelectedItem = MyStrings.ten;
+            comboBox1.SelectedIndexChanged += comboBox1_SelectedIndexChanged;
+            
+        }
+
+        private async void timer1_Tick(object sender, EventArgs e)
+        {
+            panelIndex++;
+            if (panelIndex > 3)
+            {
+                panelIndex = 1;
+            }
+            await UpdateLabels();
+        }
+
+        private async Task<string> GetWeatherData(string city)
+        {
+            try
+            {
+                string url = $"https://api.openweathermap.org/data/2.5/weather?q={city}&appid={apiKey}&units=metric";
+                var response = await httpClient.GetAsync(url);
+                response.EnsureSuccessStatusCode();
+                var content = await response.Content.ReadAsStringAsync();
+                return content;
+            }
+            catch (HttpRequestException ex)
+            {
+                MessageBox.Show($"An error occurred while getting the weather data for {city}: {ex.Message}");
+                return null;
+            }
+        }
+
+        private async Task UpdateLabels()
+        {
+            switch (panelIndex)
+            {
+                case 1:
+                    await UpdateCityWeather(MyStrings.Bengaluru);
+                    break;
+                case 2:
+                    await UpdateCityWeather(MyStrings.Varanasi);
+                    break;
+                case 3:
+                    await UpdateCityWeather(MyStrings.Madikeri);
+                    break;
+            }
+        }
+
+        private async Task UpdateCityWeather(string city)
+        {
+            var weatherData = await GetWeatherData(city);
+            if (weatherData == null)
+            {
+                return;
+            }
+            try
+            {
+                dynamic json = Newtonsoft.Json.JsonConvert.DeserializeObject(weatherData);
+                double temperature = json.main.temp;
+                double humidity = json.main.humidity;
+                double pressure = json.main.pressure;
+
+                string temperatureUnit = MyStrings.Celcius;
+                if (city == MyStrings.Varanasi)
+                {
+                    temperature = temperature * 1.8 + 32;
+                    temperatureUnit = MyStrings.Farenheit;
+                }
+                else if (city == MyStrings.Madikeri)
+                {
+                    temperature += 273.15;
+                    temperatureUnit = MyStrings.Kelvin;
+                    pressure /= 1000;
+                }
+                label1.Text = $"{city}";
+                lblHumidity.Text = $"Humidity: {humidity}%";
+                lblTemperature.Text = $"Temperature: {temperature}{temperatureUnit}";
+                lblAtmosphere.Text = $"Atmosphere: {pressure} kPa";
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"An error occurred while updating the weather data for {city}: {ex.Message}");
+            }
+        }
+
+        private async void Form1_Load(object sender, EventArgs e)
+        {
+            this.Left = WindowsFormsApp5.Properties.Settings.Default.Left;
+            this.Width = WindowsFormsApp5.Properties.Settings.Default.Width;
+            this.Height = WindowsFormsApp5.Properties.Settings.Default.Height;
+            this.Top = WindowsFormsApp5.Properties.Settings.Default.Top;
+
+            comboBox1.SelectedIndexChanged += comboBox1_SelectedIndexChanged;
+            timer1.Interval = selectedInterval;
+            timer1.Tick += async (s, ev) => await UpdateLabels();
+            timer1.Start();
+        }
+   
+        private void Form1_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            WindowsFormsApp5.Properties.Settings.Default.Left = this.Left;
+            WindowsFormsApp5.Properties.Settings.Default.Width = this.Width;
+            WindowsFormsApp5.Properties.Settings.Default.Height = this.Height;
+            WindowsFormsApp5.Properties.Settings.Default.Top = this.Top;
+            WindowsFormsApp5.Properties.Settings.Default.Save();
+        }
+            private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            switch (comboBox1.SelectedIndex)
+            {
+                case 0:
+                    selectedInterval = 5000;
+                    break;
+                case 1:
+                    selectedInterval = 10000;
+                    break;
+                case 2:
+                    selectedInterval = 15000;
+                    break;
+            }
+            timer1.Interval = selectedInterval;
+        }
+    }
+}
+
